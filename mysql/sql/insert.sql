a674e6ec3b55f6e0696d429458dfe5f1a061415a
ShotaroMatsuya <matsuya@h01.itscom.net>
2021-07-04 01:51:25 +0900

    crate book table

---
+++ mysql/sql/insert.sql
@@ -0,0 +1,6 @@
+INSERT INTO cats(name, AGE)
+VALUES('Charlie', 17);
+INSERT INTO cats(name, AGE)
+VALUES('Connie', 10);
+SELECT *
+FROM cats;
---
+++ mysql/sql/running.sql
@@ -0,0 +1,6 @@
+CREATE TABLE cats(
+    cat_id INT NOT NULL AUTO_INCREMENT,
+    name VARCHAR(100),
+    AGE INT,
+    PRIMARY KEY(cat_id)
+);
