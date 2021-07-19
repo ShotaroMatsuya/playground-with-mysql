02416863a859a93059d1d209269fa2310c22f9a8
ShotaroMatsuya <matsuya@h01.itscom.net>
2021-07-20 07:21:06 +0900

    connecting node container to mysql container

--- node/app.js
+++ node/app.js
@@ -1,14 +1,28 @@
 const faker = require('faker');
+const mysql = require('mysql');
+
+const connection = mysql.createConnection({
+  host: process.env.MYSQL_HOST,
+  user: process.env.MYSQL_USER,
+  database: process.env.MYSQL_DATABASE,
+  password: process.env.MYSQL_PASSWORD,
+});
 
 // console.log(faker.internet.email());
 // console.log(faker.date.past());
 
-function generateAddress() {
-  console.log(faker.address.streetAddress());
-  console.log(faker.address.city());
-  console.log(faker.address.state());
-}
+// function generateAddress() {
+//   console.log(faker.address.streetAddress());
+//   console.log(faker.address.city());
+//   console.log(faker.address.state());
+// }
 
-generateAddress();
-generateAddress();
-generateAddress();
+const q = 'SELECT CURTIME() as time, CURDATE() as date, NOW() as now';
+connection.query(q, function (error, results, fields) {
+  if (error) throw error;
+  console.log(results[0].time);
+  console.log(results[0].date.toString());
+  console.log(results[0].now.toString());
+});
+
+connection.end();
