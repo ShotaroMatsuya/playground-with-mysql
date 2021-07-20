5e7fac76875d9e86e17db861a7f899cbd4b5642b
ShotaroMatsuya <matsuya@h01.itscom.net>
2021-07-21 07:42:01 +0900

    building express app with ejs template

--- node/app.js
+++ node/app.js
@@ -1,61 +1,39 @@
-const faker = require('faker');
-const mysql = require('mysql');
+var express = require('express');
+var mysql = require('mysql');
+var bodyParser = require('body-parser');
+var app = express();
 
-const connection = mysql.createConnection({
+app.set('view engine', 'ejs');
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(express.static(__dirname + '/public'));
+
+var connection = mysql.createConnection({
   host: process.env.MYSQL_HOST,
   user: process.env.MYSQL_USER,
   database: process.env.MYSQL_DATABASE,
   password: process.env.MYSQL_PASSWORD,
 });
 
-// console.log(faker.internet.email());
-// console.log(faker.date.past());
-
-// function generateAddress() {
-//   console.log(faker.address.streetAddress());
-//   console.log(faker.address.city());
-//   console.log(faker.address.state());
-// }
-
-// Selecting data
-// const q = 'SELECT COUNT(*) AS total FROM users';
-// connection.query(q, function (error, results, fields) {
-//   if (error) throw error;
-//   console.log(results[0].total);
-// });
-// Inserting data
-// const q = 'INSERT INTO users(email) VALUES("wyatt_the_dog@gmail.com")';
-// connection.query(q, function (error, results, fields) {
-//   if (error) throw error;
-//   console.log(results);
-// });
-// Inserting data take 2
-// const person = { email: faker.internet.email(), created_at: faker.date.past() };
-
-// const end_result = connection.query(
-//   'INSERT INTO users SET ?',
-//   person,
-//   function (err, result) {
-//     if (err) throw err;
-//     console.log(result);
-//   }
-// );
-// console.log(end_result.sql);
-
-// connection.end();
-
-// console.log(faker.date.past());
-// 2020-09-03T15:04:46.312Z
-
-// Insert lots of data!!!
-const data = [];
-for (let i = 0; i < 500; i++) {
-  data.push([faker.internet.email(), faker.date.past()]);
-}
-console.log(data);
-const q = 'INSERT INTO users(email,created_at) VALUES ?';
-connection.query(q, [data], function (err, result) {
-  console.log(err);
-  console.log(result);
+app.get('/', function (req, res) {
+  // Find count of users in DB
+  var q = 'SELECT COUNT(*) AS count FROM users';
+  connection.query(q, function (err, results) {
+    if (err) throw err;
+    var count = results[0].count;
+    res.render('home', { count: count });
+  });
 });
-connection.end();
+
+app.post('/register', function (req, res) {
+  var person = {
+    email: req.body.email,
+  };
+  connection.query('INSERT INTO users SET ?', person, function (err, result) {
+    if (err) throw err;
+    res.redirect('/');
+  });
+});
+
+app.listen(3000, function () {
+  console.log('Server running on 3000!');
+});
